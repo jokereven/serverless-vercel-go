f9be892babb5d1d25f11ce3fa5075a4c76b1cd57
周靖 <zjing125.cn@gmail.com>
2023-03-03 12:18:29 +0800

    Update and rename date.py to blog.py

---
+++ api/blog.py
@@ -0,0 +1,29 @@
+import requests
+import urllib3
+import xmltodict
+import json
+
+from http.server import BaseHTTPRequestHandler
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    'Accept': '*/*',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+}
+
+
+class handler(BaseHTTPRequestHandler):
+    def blog(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        url = 'https://blog.code520.com.cn/search.xml'
+        response = requests.get(
+            url, headers=headers, verify=False)
+        if response.status_code == 200:
+            xp = xmltodict.parse(response.text)
+            data = json.dumps(xp)
+            self.wfile.write(data)
+            return
