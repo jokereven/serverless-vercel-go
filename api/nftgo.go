2f64f72362c3dbb40942cda78358bc82c410cefd
zhou jing <zjing125.cn@gmail.com>
2023-07-19 08:59:16 +0800

    Create nftgo.go

---
+++ api/nftgo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+)
+
+func main() {
+    client := &http.Client{}
+    req, err := http.NewRequest("GET", "https://api.nftgo.io/api/v1/asset/eth-usd-rate", nil)
+    if err != nil {
+        panic(err)
+    }
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(string(body))
+}
