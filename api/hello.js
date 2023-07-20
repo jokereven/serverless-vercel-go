1f702cbd10846fe8bbc4444cc768ffdf02c40553
jokereven <zjing125.cn@gmail.com>
2023-07-20 10:16:12 +0800

    feat(htllo.js) add axios in node

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,17 @@
+import axios from 'axios';
+
 export default function handler(request, response) {
-	const { name } = request.query;
-	response.status(200).send(`Hello ${name}!`);
+  // api/[name].js -> /api/lee
+  // request.query.name -> "lee"
+  const { name } = request.query;
+
+  axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => {
+      console.log(res.data);
+      return response.end(`Hello ${name}!`);
+    })
+    .catch(err => {
+      console.error(err);
+      return response.end(`Hello ${name}!`);
+    });
 }
